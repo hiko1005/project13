647032caac3b6be712ce45b848ff6066a55b1ae1
Vlad Kowanovkyi <kvladik61@gmail.com>
2023-12-09 17:25:34 +0300

    first commit

--- script/script.js
+++ script/script.js
@@ -11,13 +11,13 @@ function navControll(info) {
     let prev = document.querySelector("#prev");
     let next = document.querySelector("#next");
 
-    if(info.next_url != undefined && info.next_url != null) {
+    if (info.next_url != undefined && info.next_url != null) {
         next.removeAttribute("hidden");
         next.setAttribute("url", info.next_url)
     } else {
         next.setAttribute("hidden");
     }
-    if(info.prev_url != undefined && info.prev_url != null) {
+    if (info.prev_url != undefined && info.prev_url != null) {
         prev.removeAttribute("hidden");
         prev.setAttribute("url", info.prev_url)
     } else {
@@ -25,22 +25,37 @@ function navControll(info) {
     }
 }
 
+function createPattern() {
+    let pattern = `
+    <p class="m-2">{{artist_display}}</p>
+    <p class="m-2">{{place_of_origin}}</p>
+    <p class="m-2">{{publication_history}}</p>
+    <p class="m-2">{{description}}</p>
+    <p class="m-2">{{exhibition_history}}</p>
+    <p class="m-2">{{copyright_notice}}</p>
+    <button class="m-2 btn btn-primary api_btn" url="{{api_link}}" onclick="openApiLink(e)">Link</button>
+    `;
+    return pattern
+}
+
+function openApiLink(e) {
+    root.innerHTML = "";
+    
+}
+
 function createContentItem(item) {
-    let div =  document.createElement("div");
+    let div = document.createElement("div");
     div.classList.add("card");
 
-    let displayAttributes = ['artist_display', 'place_of_origin']
+    let div_ = createPattern();
 
     Object.keys(item).forEach(key => {
-        if(displayAttributes.includes(key)) {
-        let p = document.createElement("p");
-        p.innerText = item[key];
-        div.appendChild(p);
-        }
+            div_ = div_.replace("{{"+key+"}}", item[key] ?? "");
     })
-
-
-    root.appendChild(div)
+    let info = document.createElement("div");
+    info.innerHTML = div_;
+    div.appendChild(info);
+    root.appendChild(div);
 }
 
 function doRequest(url) {
@@ -69,6 +84,8 @@ document.querySelector("#btn").addEventListener("click", (e) => {
     navHandleClick(e)
     // doRequest("https://api.artic.edu/api/v1/artworks");
 })
+
+
 
 document.querySelector("#prev").addEventListener("click", (e) => navHandleClick(e))
 document.querySelector("#next").addEventListener("click", (e) => navHandleClick(e))
